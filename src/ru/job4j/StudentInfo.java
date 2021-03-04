466cf6209767d214807b30e74c964a1836b7a33d
axelmark <axelmark@mail.ru>
2021-03-04 18:49:41 +0300

    3.3. Разбор первой программы. [#486891]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("axelmark");
+        System.out.println("19.06.2021");
+    }
+
+}
