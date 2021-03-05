617a813a4811af705a84ffc4536dcd9385459635
axelmark <axelmark@mail.ru>
2021-03-05 19:52:38 +0300

    7. Расстояние между точками в системе координат [#486885]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,13 +2,15 @@ package ru.job4j.condition;
 
 public class Greeting {
 
-    public static void main(String[] args) {
+    @SuppressWarnings("checkstyle:LeftCurly")
+    public static void main(String[] args)
+    {
         String idea = "I like Java!";
         int year = 2021;
 
         idea += " But I am a newbie.";
         idea += year;
         System.out.println(idea);
-    }
 
+    }
 }
