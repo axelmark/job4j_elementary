3a03a19c053d35cdd632340ff991f98a4304431f
axelmark <axelmark@mail.ru>
2021-03-08 14:13:55 +0300

    4.3.4. if c return [#486826]

--- src/ru/job4j/condition/PointEq.java
+++ src/ru/job4j/condition/PointEq.java
@@ -9,7 +9,8 @@ public class PointEq {
     }
 
     public static void main(String[] args) {
-        System.out.println(eq(1, 1, 2, 2));
+        System.out.println(eq(2, 2, 2, 2));
         System.out.println(eq(10, 10, 10, 10));
     }
+
 }
