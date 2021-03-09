60171053c5445a78d05fdd9862135ec8dadf50d2
axelmark <axel@mail.ru>
2021-03-09 14:55:23 +0300

    4.3.6. Math.abs [#486830]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -2,14 +2,10 @@ package ru.job4j.condition;
 
 public class ChessBoard {
 
-    public static int[] way(int x1, int y1, int x2, int y2) {
-        int[] rsl = new int[2];
-        if (x1 != x2 && y1 != y2) {
-            rsl[0] = Math.abs(x2 - x1);
-            rsl[1] = Math.abs(y2 - y1);
-        } else {
-            return rsl;
+    public static int way(int x1, int y1, int x2, int y2) {
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+            return Math.abs(x2 - x1);
         }
-        return rsl;
+        return 0;
     }
 }
--- test/ru/job4j/condition/ChessBoardTest.java
+++ test/ru/job4j/condition/ChessBoardTest.java
@@ -9,14 +9,13 @@ public class ChessBoardTest {
 
     @Test
     public void wayX2Y2() {
-        int[] way = ChessBoard.way(5, 3, 7, 5);
-        assertThat(way[0], is(2));
-        assertThat(way[1], is(2));
+        int way = ChessBoard.way(5, 3, 7, 5);
+        assertThat(way, is(2));
     }
 
     @Test
     public void wayIs5() {
-        int[] way = ChessBoard.way(0, 0, 5, 0);
-        assertThat(way[0], is(0));
+        int way = ChessBoard.way(0, 0, 5, 0);
+        assertThat(way, is(0));
     }
 }
