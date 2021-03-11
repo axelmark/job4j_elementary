a503c4e0e59e7a82bb315d99b64a8220e34d30ba
axelmark <axelmark@mail.ru>
2021-03-11 16:08:19 +0300

    5.5. Простое число [#486931]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -28,6 +28,7 @@ public class Counter {
 
     public static void main(String[] args) {
 //        System.out.println(sumByEven(1, 10));
+
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
