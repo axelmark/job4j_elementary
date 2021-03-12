144deee1702b7428a5589ab474e898d4fa1aad6d
axelmark <axelmark@mail.ru>
2021-03-12 20:14:30 +0300

    5.5. Простое число [#486931]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,11 +3,13 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-        for (int i = 2; i <= number / 2; i++) {
+        boolean result = number > 1;
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                return false;
+                result = false;
+                break;
             }
         }
-        return true;
+        return result;
     }
 }
